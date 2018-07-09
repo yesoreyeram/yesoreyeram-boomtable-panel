5492b11f3e9b2515fef43ca5a5a6f91843c83295
Sriramajeyam Sugumaran <S.Sugumaran@asos.com>
2018-07-09 11:55:31 +0100

    Clone pattern

--- src/module.ts
+++ src/module.ts
@@ -66,6 +66,11 @@ class GrafanaBoomTableCtrl extends MetricsPanelCtrl {
     this.panel.activePatternIndex = (this.panel.patterns && this.panel.patterns.length > 0) ? (this.panel.patterns.length - 1) : -1;
     this.render();
   }
+  clonePattern(index){
+    let copiedPattern = Object.assign( {} , this.panel.patterns[index] );
+    this.panel.patterns.push(copiedPattern);
+    this.render();
+  }
   inverseBGColors(index){
     this.panel.patterns[index].bgColors = this.panel.patterns[index].bgColors.split("|").reverse().join("|");
     this.render();
