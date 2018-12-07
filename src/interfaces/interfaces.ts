e5f70fba671ac619625018f40e13f6a36fc19e0c
s.sugumaran@asos.com <yesoreyeram@gmail.com>
2018-12-07 17:26:25 +0000

    interfaces implementation

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,15 +1,15 @@
 export interface PanelDefault {
     currentOptionOverrides: any,
-    patterns:any,
-    defaultPattern:any,
-    activePatternIndex : Number,
+    patterns: any,
+    defaultPattern: any,
+    activePatternIndex: Number,
     row_col_wrapper: String,
-    default_title_for_rows:String
+    default_title_for_rows: String
 }
-export interface  Config {
-    plugin_id : String,
-    debug_mode : Boolean,
-    error : Error,
+export interface Config {
+    plugin_id: String,
+    debug_mode: Boolean,
+    error: Error,
     groupedData: any,
     optionOverrides: any,
     panelDefaults: PanelDefault,
