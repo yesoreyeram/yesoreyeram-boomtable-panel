f50fc9c62142cc092eaa8ca54a6a0d66e89716c6
Sriramajeyam <yesoreyeram@gmail.com>
2018-12-13 16:33:32 +0000

    Checking jest

--- src/app/renderer.ts
+++ src/app/renderer.ts
@@ -72,7 +72,7 @@ let output = function (output: any[], cols_found: any, options: any): { header: 
             <div
             data-toggle="tooltip"
             data-html="true"
-            data-placement="left"
+            data-placement="center"
             title="${c.tooltip}"
             style="padding-left:10px">
                 ${c.displayValue}
